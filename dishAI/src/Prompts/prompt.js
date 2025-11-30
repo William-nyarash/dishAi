d76b3d18430c15c9922bda160f80ff6981f8dfdf
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-11-30 10:22:56 +0300

    sample prompt

---
+++ dishAI/src/Prompts/prompt.js
@@ -0,0 +1,7 @@
+export const CookingPrompt =(ingredients) => {
+
+    return `You are a resourceful AI chef.
+    you only have this ingredients: ${ingredients} 
+    Suggest one simple , beginner-friendly meal I can cook.
+    Respond concisely in 1 paragraph.`;
+}
